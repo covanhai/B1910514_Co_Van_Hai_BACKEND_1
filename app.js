b3fb5716c1007c5251009ed696c6075b58a6727d
covanhai <an4294205@gmail.com>
2024-11-27 22:15:25 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 
+const ApiError = require("./app/api-error");
 const contactsRouter = require("./app/routes/contact.route");
+
 
 const app = express();
 
@@ -15,5 +17,16 @@ app.get('/', (req, res) => {
 })
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 
 module.exports = app;
